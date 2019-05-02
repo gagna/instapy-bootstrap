2358ad9ecc6da9c7bff197002f854f3b0c30ca2e
gagna <gagna90@gmail.com>
2019-05-02 19:54:44 +0200

    Moved txt files to subfolder

---
+++ prova.py
@@ -0,0 +1,3 @@
+friends_list = [line.rstrip("\n") for line in open("txt_files/blacktags.txt")]
+
+print(friends_list[2])
--- quickstart.py
+++ quickstart.py
@@ -16,17 +16,17 @@ insta_username = ''  # <- enter username here
 insta_password = ''  # <- enter password here
 
 # populate list from file
-friends_list = [line.rstrip("\n") for line in open("friends.txt")]
-influencers_list = [line.rstrip("\n") for line in open("influencers.txt")]
-blacktags_list = [line.rstrip("\n") for line in open("blacktags.txt")]
-locations_list = [line.rstrip("\n") for line in open("locations.txt")]
-hashtags_list = [line.rstrip("\n") for line in open("hashtags.txt")]
-smart_hashtags_list = [line.rstrip("\n") for line in open("smart_hashtags.txt")]
-smart_locations_list = [line.rstrip("\n") for line in open("smart_locations.txt")]
-pic_comments_list = [line.rstrip("\n") for line in open("pic_comments.txt")]
-vid_comments_list = [line.rstrip("\n") for line in open("vid_comments.txt")]
-mandatory_words_list = [line.rstrip("\n") for line in open("mandatory_words.txt")]
-pods_comments_list = [line.rstrip("\n") for line in open("pods_comments.txt")]
+friends_list = [line.rstrip("\n") for line in open("txt_files/friends.txt")]
+influencers_list = [line.rstrip("\n") for line in open("txt_files/influencers.txt")]
+blacktags_list = [line.rstrip("\n") for line in open("txt_files/blacktags.txt")]
+locations_list = [line.rstrip("\n") for line in open("txt_files/locations.txt")]
+hashtags_list = [line.rstrip("\n") for line in open("txt_files/hashtags.txt")]
+smart_hashtags_list = [line.rstrip("\n") for line in open("txt_files/smart_hashtags.txt")]
+smart_locations_list = [line.rstrip("\n") for line in open("txt_files/smart_locations.txt")]
+pic_comments_list = [line.rstrip("\n") for line in open("txt_files/pic_comments.txt")]
+vid_comments_list = [line.rstrip("\n") for line in open("txt_files/vid_comments.txt")]
+mandatory_words_list = [line.rstrip("\n") for line in open("txt_files/mandatory_words.txt")]
+pods_comments_list = [line.rstrip("\n") for line in open("txt_files/pods_comments.txt")]
 
 # ------------------------------------------------------------------------
 

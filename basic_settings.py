4a92eb9ea0d0524d496b821e1f3f6db1ea1c12ee
gagna <gagna90@gmail.com>
2019-05-05 19:08:48 +0200

    Converted list to tuples

--- basic_settings.py
+++ basic_settings.py
@@ -7,17 +7,15 @@ insta_username = ""  # <- enter username here
 insta_password = ""  # <- enter password here
 
 # populate list from file
-friends_list = [line.rstrip() for line in open("txt_files/friends.txt")]
-influencers_list = [line.rstrip() for line in open("txt_files/influencers.txt")]
-blacktags_list = [line.rstrip() for line in open("txt_files/blacktags.txt")]
-locations_list = [line.rstrip() for line in open("txt_files/locations.txt")]
-hashtags_list = [line.rstrip() for line in open("txt_files/hashtags.txt")]
-smart_hashtags_list = [line.rstrip() for line in open("txt_files/smart_hashtags.txt")]
-smart_locations_list = [line.rstrip() for line in open("txt_files/smart_locations.txt")]
-pic_comments_list = [line.rstrip() for line in open("txt_files/pic_comments.txt")]
-vid_comments_list = [line.rstrip() for line in open("txt_files/vid_comments.txt")]
-mandatory_words_list = [line.rstrip() for line in open("txt_files/mandatory_words.txt")]
-pods_comments_list = [line.rstrip() for line in open("txt_files/pods_comments.txt")]
-white_list = [line.rstrip() for line in open("txt_files/whitelist.txt")]
-
-print(blacktags_list[12])
+friends_list = tuple(line.rstrip() for line in open("txt_files/friends.txt"))
+influencers_list = tuple(line.rstrip() for line in open("txt_files/influencers.txt"))
+blacktags_list = tuple(line.rstrip() for line in open("txt_files/blacktags.txt"))
+locations_list = tuple(line.rstrip() for line in open("txt_files/locations.txt"))
+hashtags_list = tuple(line.rstrip() for line in open("txt_files/hashtags.txt"))
+smart_hashtags_list = tuple(line.rstrip() for line in open("txt_files/smart_hashtags.txt"))
+smart_locations_list = tuple(line.rstrip() for line in open("txt_files/smart_locations.txt"))
+pic_comments_list = tuple(line.rstrip() for line in open("txt_files/pic_comments.txt"))
+vid_comments_list = tuple(line.rstrip() for line in open("txt_files/vid_comments.txt"))
+mandatory_words_list = tuple(line.rstrip() for line in open("txt_files/mandatory_words.txt"))
+pods_comments_list = tuple(line.rstrip() for line in open("txt_files/pods_comments.txt"))
+white_list = tuple(line.rstrip() for line in open("txt_files/whitelist.txt"))
